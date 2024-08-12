c24a72abc8fa6b6820085163373be9d51ae16194
Khen Channmakara <khenchannmakara@gmail.com>
2024-08-13 00:17:02 +0700

    Chang icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </head>
       <body className={inter.className}>
         <Provider>
           <Navbar />
